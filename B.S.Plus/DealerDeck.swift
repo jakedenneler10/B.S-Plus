14c0f67f8bda1b172f6911fb0ebbab3e7d2ff292
Jacob Hall <jacob.hall@jhnw.edu>
2024-10-22 10:34:53 -0600

    DealerDeck

---
+++ B.S.Plus/DealerDeck.swift
@@ -0,0 +1,25 @@
+//
+//  Deck.swift
+//  B.S.Plus
+//
+//  Created by Student on 10/22/24.
+//
+
+import Foundation
+
+struct DealerDeck : Deck{
+	var cards: [DealerCard]
+	func shuffle() {
+		<#code#>
+	}
+	
+	func reset() {
+		<#code#>
+	}
+	
+	func drawACard() {
+		<#code#>
+	}
+	
+	
+}
