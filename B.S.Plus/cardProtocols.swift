cdc43a62c75ca5a99e0bc0869022deeaba1f1fcd
Jacob Hall <jacob.hall@jhnw.edu>
2024-10-22 09:56:59 -0600

    Add the Card Protocol

---
+++ B.S.Plus/cardProtocols.swift
@@ -0,0 +1,22 @@
+//
+//  cardProtocols.swift
+//  B.S.Plus
+//
+//  Created by Student on 10/22/24.
+//
+
+import Foundation
+
+protocol Card {}
+
+protocol Deck {
+	func shuffle()
+	func reset()
+	func drawACard()
+	
+}
+
+protocol Shoe {
+	func reset()
+	func drawACard()
+}
