a1f0e30e41dcac0a94ad8efddff62b3a356e0eba
Karson Mellott <karson.mellott@fhnw.edu>
2024-10-22 10:29:16 -0600

    Added Shoe

---
+++ B.S.Plus/DealerShoe.swift
@@ -0,0 +1,22 @@
+//
+//  DealerShoe.swift
+//  B.S.Plus
+//
+//  Created by Karson Mellott on 10/22/24.
+//
+
+import Foundation
+
+var cardDecks : Array = [DealerDeck]
+
+struct DealerShoe : Shoe {
+    func reset() {
+        
+    }
+    
+    func drawACard() {
+        
+    }
+    
+    
+}
