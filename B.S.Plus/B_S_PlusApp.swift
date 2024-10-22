889935867d85a9e48ebd46b68d5c76d5171b9b6e
Jacob Hall <jacob.hall@jhnw.edu>
2024-10-22 08:39:52 -0600

    Initial Commit

---
+++ B.S.Plus/B_S_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  B_S_PlusApp.swift
+//  B.S.Plus
+//
+//  Created by Student on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct B_S_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ B.S.PlusTests/B_S_PlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  B_S_PlusTests.swift
+//  B.S.PlusTests
+//
+//  Created by Student on 10/22/24.
+//
+
+import Testing
+@testable import B_S_Plus
+
+struct B_S_PlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
