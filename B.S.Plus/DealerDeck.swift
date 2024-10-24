d5e9ac531e5033f121f3a23da58b01bc63ae0678
Kayden <kayden.davis@fhnw.edu>
2024-10-24 08:41:42 -0600

    Added all the cards

--- B.S.Plus/DealerDeck.swift
+++ B.S.Plus/DealerDeck.swift
@@ -8,17 +8,135 @@
 import Foundation
 
 struct DealerDeck : Deck{
-	var cards: [DealerCard]
+	var cards: [DealerCard] = []
+    var deck = 54
+    
+    init() {
+        let c1 = DealerCard(suit: "Spades", value: 1)
+        cards.append(c1)
+        let c2 = DealerCard(suit: "Spades", value: 2)
+        cards.append(c2)
+        let c3 = DealerCard(suit: "Spades", value: 3)
+        cards.append(c3)
+        let c4 = DealerCard(suit: "Spades", value: 4)
+        cards.append(c4)
+        let c5 = DealerCard(suit: "Spades", value: 5)
+        cards.append(c5)
+        let c6 = DealerCard(suit: "Spades", value: 6)
+        cards.append(c6)
+        let c7 = DealerCard(suit: "Spades", value: 7)
+        cards.append(c7)
+        let c8 = DealerCard(suit: "Spades", value: 8)
+        cards.append(c8)
+        let c9 = DealerCard(suit: "Spades", value: 9)
+        cards.append(c9)
+        let c10 = DealerCard(suit: "Spades", value: 10)
+        cards.append(c10)
+        let c11 = DealerCard(suit: "Spades", value: 11)
+        cards.append(c11)
+        let c12 = DealerCard(suit: "Spades", value: 12)
+        cards.append(c12)
+        let c13 = DealerCard(suit: "Spades", value: 13)
+        cards.append(c13)
+        let c14 = DealerCard(suit: "Diamonds", value: 1)
+        cards.append(c14)
+        let c15 = DealerCard(suit: "Diamonds", value: 2)
+        cards.append(c15)
+        let c16 = DealerCard(suit: "Diamonds", value: 3)
+        cards.append(c16)
+        let c17 = DealerCard(suit: "Diamonds", value: 4)
+        cards.append(c17)
+        let c18 = DealerCard(suit: "Diamonds", value: 5)
+        cards.append(c18)
+        let c19 = DealerCard(suit: "Diamonds", value: 6)
+        cards.append(c19)
+        let c20 = DealerCard(suit: "Diamonds", value: 7)
+        cards.append(c20)
+        let c21 = DealerCard(suit: "Diamonds", value: 8)
+        cards.append(c21)
+        let c22 = DealerCard(suit: "Diamonds", value: 9)
+        cards.append(c22)
+        let c23 = DealerCard(suit: "Diamonds", value: 10)
+        cards.append(c23)
+        let c24 = DealerCard(suit: "Diamonds", value: 11)
+        cards.append(c24)
+        let c25 = DealerCard(suit: "Diamonds", value: 12)
+        cards.append(c25)
+        let c26 = DealerCard(suit: "Diamonds", value: 13)
+        cards.append(c26)
+        let c27 = DealerCard(suit: "Clubs", value: 1)
+        cards.append(c27)
+        let c28 = DealerCard(suit: "Clubs", value: 2)
+        cards.append(c28)
+        let c29 = DealerCard(suit: "Clubs", value: 3)
+        cards.append(c29)
+        let c30 = DealerCard(suit: "Clubs", value: 4)
+        cards.append(c30)
+        let c31 = DealerCard(suit: "Clubs", value: 5)
+        cards.append(c31)
+        let c32 = DealerCard(suit: "Clubs", value: 6)
+        cards.append(c32)
+        let c33 = DealerCard(suit: "Clubs", value: 7)
+        cards.append(c33)
+        let c34 = DealerCard(suit: "Clubs", value: 8)
+        cards.append(c34)
+        let c35 = DealerCard(suit: "Clubs", value: 9)
+        cards.append(c35)
+        let c36 = DealerCard(suit: "Clubs", value: 10)
+        cards.append(c36)
+        let c37 = DealerCard(suit: "Clubs", value: 11)
+        cards.append(c37)
+        let c38 = DealerCard(suit: "Clubs", value: 12)
+        cards.append(c38)
+        let c39 = DealerCard(suit: "Clubs", value: 13)
+        cards.append(c39)
+        let c40 = DealerCard(suit: "Hearts", value: 1)
+        cards.append(c40)
+        let c41 = DealerCard(suit: "Hearts", value: 2)
+        cards.append(c41)
+        let c42 = DealerCard(suit: "Hearts", value: 3)
+        cards.append(c42)
+        let c43 = DealerCard(suit: "Hearts", value: 4)
+        cards.append(c43)
+        let c44 = DealerCard(suit: "Hearts", value: 5)
+        cards.append(c44)
+        let c45 = DealerCard(suit: "Hearts", value: 6)
+        cards.append(c45)
+        let c46 = DealerCard(suit: "Hearts", value: 7)
+        cards.append(c46)
+        let c47 = DealerCard(suit: "Hearts", value: 8)
+        cards.append(c47)
+        let c48 = DealerCard(suit: "Hearts", value: 9)
+        cards.append(c48)
+        let c49 = DealerCard(suit: "Hearts", value: 10)
+        cards.append(c49)
+        let c50 = DealerCard(suit: "Hearts", value: 11)
+        cards.append(c50)
+        let c51 = DealerCard(suit: "Hearts", value: 12)
+        cards.append(c51)
+        let c52 = DealerCard(suit: "Hearts", value: 13)
+        cards.append(c52)
+        let c53 = DealerCard(suit: "Joker", value: 0)
+        cards.append(c53)
+        let c54 = DealerCard(suit: "Joker", value: 0)
+        cards.append(c54)
+        
+        
+        
+        
+        
+    }
+    
 	func shuffle() {
-		<#code#>
+		
 	}
 	
 	func reset() {
-		<#code#>
+		
 	}
 	
 	func drawACard() {
-		<#code#>
+		
 	}
 	
 	
